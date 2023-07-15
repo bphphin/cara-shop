e857a5363038faaa1d18f5a18cc57ae408660a02
bphphin <buingocphinn@gmail.com>
2023-07-15 10:40:41 +0700

    chore: fix ui

--- resources/views/clients/layouts/header.blade.php
+++ resources/views/clients/layouts/header.blade.php
@@ -1,9 +1,8 @@
 <section id="header">
-    <a href="" class="logo"><img src="{{ asset('assets/imgs/logo.png') }}" alt=""></a>
-
-    <div>
+    <a href="{{ route('home-client') }}" class="logo"><img src="{{ asset('assets/imgs/logo.png') }}" alt=""></a>
+    <div class="">
         <ul id="navbar">
-            <li><a class="active" href="index.html">Home</a></li>
+            <li><a class="active" href="{{ route('home-client') }}">Home</a></li>
             <li><a href="shop.html">Shop</a></li>
             <li><a href="blog.html">Blog</a></li>
             <li><a href="about.html">About</a></li>
@@ -12,8 +11,28 @@
         </ul>
     </div>
     <div class="control">
-        <a href="./model/login.html">Log In</a>
-        <a href="./model/singin.html">Sign Up</a>
+        @if(Auth::check())
+        {{-- <div class="dropdown">
+            <p class="text-uppercase dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</p>
+            <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>
+            </ul>
+        </div> --}}
+        <div class="d-flex align-items-center">
+            <div>
+                <p class="text-uppercase my-2">{{ Auth::user()->name }}</p>
+            </div>
+            <div>
+                <a href="{{ route('auth.logout') }}" class="text-decoration-none">Logout</a>
+            </div>
+        </div>
+        @else
+        <a href="{{ route('auth.loginForm') }}">Log In</a>
+        <a href="{{ route('auth.registerForm') }}">Sign Up</a>
+        @endif
     </div>
 
     <div class="mobile">
