1ae08ef459af9298aa18df79c91f51dcd9d1bb89
bphphin <buingocphinn@gmail.com>
2023-07-19 21:25:17 +0700

    fix: toastr message auth

--- app/Http/Controllers/Clients/AuthController.php
+++ app/Http/Controllers/Clients/AuthController.php
@@ -7,40 +7,52 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Clients\Auth\AuthRequest;
+use Brian2694\Toastr\Facades\Toastr as FacadesToastr;
+use Brian2694\Toastr\Toastr;
 
 class AuthController extends Controller
 {
-    public function loginForm() {
+    public function loginForm()
+    {
         return view('clients.pages.auth.login');
     }
-    public function login(Request $request) {
-        $infoLogin = $request->only('email','password');
-        if(Auth::attempt($infoLogin)) {
-            toastr()->success('Đăng nhập thành công');
+    public function login(Request $request)
+    {
+        $infoLogin = $request->only('email', 'password');
+        if (Auth::attempt($infoLogin)) {
+            if($this) {
+
+            }
+            toastr()->success('Đăng nhập thành công', 'Thành công');
+            // Toastr::success('Đăng nhập thành công', 'Thành công');
             return redirect()->route('home-client');
         }
-        toastr()->error('Đăng nhập thất bại');
+        toastr()->error('Đăng nhập thất bại', 'Lỗi');
         return back();
     }
-    public function registerForm() {
+    public function registerForm()
+    {
         return view('clients.pages.auth.register');
     }
-    public function register(Request $request) {
+    public function register(AuthRequest $request)
+    {
         $infoRegister = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ])->save();
-        if($infoRegister){
-            toastr()->success('Đăng ký thành công');
+        if ($infoRegister) {
+            toastr()->success('Đăng ký thành công', 'Thành công');
             return redirect()->route('auth.loginForm');
         }
-        toastr()->error('Đăng ký không thành công');
+        toastr()->error('Đăng ký không thành công', 'Lỗi');
         return back();
     }
-    public function logout() {
+    public function logout()
+    {
         Auth::logout();
-        toastr()->info('Logout Successfully');
+        toastr()->info('Logout Successfully', 'Thành công');
         return redirect()->route('auth.loginForm');
     }
 }
