648e5d0d4c47194452c99cd9a7ce784bdef18449
bphphin <buingocphinn@gmail.com>
2023-07-22 18:44:39 +0700

    fix(ui): update nav admin

--- resources/views/clients/layouts/header.blade.php
+++ resources/views/clients/layouts/header.blade.php
@@ -12,26 +12,38 @@
     </div>
     <div class="control">
         @if(Auth::check())
-        {{-- <div class="dropdown">
-            <p class="text-uppercase dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</p>
-            <span class="caret"></span></button>
-            <ul class="dropdown-menu">
-                <li><a href="#">HTML</a></li>
-                <li><a href="#">CSS</a></li>
-                <li><a href="#">JavaScript</a></li>
-            </ul>
-        </div> --}}
-        <div class="d-flex align-items-center">
-            <div>
-                <p class="text-uppercase my-2">{{ Auth::user()->name }}</p>
-            </div>
-            <div>
-                <a href="{{ route('auth.logout') }}" class="text-decoration-none">Logout</a>
-            </div>
-        </div>
+            {{-- <div class="dropdown">
+                <p class="text-uppercase dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</p>
+                <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">CSS</a></li>
+                    <li><a href="#">JavaScript</a></li>
+                </ul>
+            </div> --}}
+            @if(Auth::user()->role === 1)
+                <div class="d-flex align-items-center">
+                    <div>
+                        <p class="text-uppercase my-2">{{ Auth::user()->name }}</p>
+                    </div>
+                    <div>
+                        <a href="{{ route('admin') }}" class="text-decoration-none">Dashboard</a>
+                    </div>
+                </div>
+            @else
+                <div class="d-flex align-items-center">
+                    <div>
+                        <p class="text-uppercase my-2">{{ Auth::user()->name }}</p>
+                    </div>
+                    <div>
+                        <a href="{{ route('auth.logout') }}" class="text-decoration-none">Logout</a>
+                    </div>
+                </div>
+            @endif
+
         @else
-        <a href="{{ route('auth.loginForm') }}">Log In</a>
-        <a href="{{ route('auth.registerForm') }}">Sign Up</a>
+            <a href="{{ route('auth.loginForm') }}">Log In</a>
+            <a href="{{ route('auth.registerForm') }}">Sign Up</a>
         @endif
     </div>
 
