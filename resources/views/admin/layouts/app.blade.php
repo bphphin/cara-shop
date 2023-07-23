0709852ac08e7e430a25f5830c9ec6b9f7248615
bphphin <buingocphinn@gmail.com>
2023-07-23 15:49:08 +0700

    fix(ui): update master layout dashboard

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,17 +1,21 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Dashboard Page')</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('assets/imgs/clothes-hanger.png') }}" type="image/gif" sizes="16x16">
+    <!----======== CSS ======== -->
     <link rel="stylesheet" href="{{ asset('assets/css/admin/dashboard.css')  }}">
+
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>@yield('title','Dashboard')</title>
 </head>
 <body>
-
-    @include('admin.layouts.header')
-    @yield('app')
-    @include('admin.layouts.footer')
+@include('admin.layouts.header')
+@yield('app')
+<script src="{{ asset('assets/js/admin/dashboard.js')  }}"></script>
 </body>
 </html>
