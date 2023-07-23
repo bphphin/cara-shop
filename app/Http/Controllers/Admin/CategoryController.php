b068ce16067d479d0d39384da97875d607a3b510
bphphin <buingocphinn@gmail.com>
2023-07-24 01:50:48 +0700

    feat: step 1 create Category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,19 +5,34 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\Admin\CategoryRequest;
 
 class CategoryController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $cates = Category::all();
-        return view('admin.pages.categories.index',compact('cates'));
+        return view('admin.pages.categories.index', compact('cates'));
     }
 
-    public function trash() {
+    public function trash()
+    {
         $cates = Category::onlyTrashed()->get();
-        return view('admin.pages.categories.trash-list',compact('cates'));
+        return view('admin.pages.categories.trash-list', compact('cates'));
     }
 
-    public function create() {
+    public function create()
+    {
+        return view('admin.pages.categories.create-form');
+    }
+
+    public function store(CategoryRequest $request)
+    {
+        $isSuccess = Category::create([
+            'name' => $request->name,
+            'slug' => $request->slug ?? '',
+            'description' => $request->description ?? ''
+        ]);
+         return checkEndDisplayMsg($isSuccess,'Thêm mới thành công','success','admin.category.index');
     }
 }
