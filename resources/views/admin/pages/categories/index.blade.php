5cb5b7c321ccbf449764be09a1d4a4a6976517fc
bphphin <buingocphinn@gmail.com>
2023-07-24 01:09:56 +0700

    fix: route add category and trash-list Category

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -3,12 +3,23 @@
     <div class="dash-content">
         <div class="activity">
             <div class="py-20">
+                <div class="flex justify-end my-2">
+                    <a href="{{ route('admin.category.create')  }}"
+                       class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-2 rounded-full">+ New
+                        Category</a>
+                    <a
+                        class="bg-[#f687b3] hover:bg-[#f687b3] text-white font-bold py-2 px-4 rounded-full"
+                        href="{{ route('admin.category.trash')  }}">
+                        Trash
+                        <i class="fa-solid fa-trash"></i>
+                    </a>
+                </div>
                 <div class="relative overflow-x-auto">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3">
-                                Category Name
+                                Name
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Slug
@@ -18,12 +29,6 @@
                             </th>
                             <th scope="col" class="px-6 py-3 w-[300px]">
                                 Action
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                                    Trace
-                                    <i class="fa-solid fa-triangle-exclamation"></i>
-                                </button>
                             </th>
                         </tr>
                         </thead>
@@ -42,9 +47,11 @@
                                 <td class="px-6 py-4">
                                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                                         Edit
+                                        <i class="fa-solid fa-pen"></i>
                                     </button>
                                     <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
                                         Remove
+                                        <i class="fa-solid fa-xmark"></i>
                                     </button>
                                 </td>
                             </tr>
