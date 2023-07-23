17dc59e134cf266648db653acbad5ee1467eadcc
bphphin <buingocphinn@gmail.com>
2023-07-24 01:52:38 +0700

    fix(ui): update label form from vietnamese to E

--- resources/views/admin/pages/brands/create-form.blade.php
+++ resources/views/admin/pages/brands/create-form.blade.php
@@ -11,7 +11,7 @@
                                placeholder=" " value="{{ old('name')  }}"/>
                         <label for="floating_email"
                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tên
-                            danh mục</label>
+                            thương hiệu</label>
                         @error('name')
                             <p class="my-2 text-red-400">{{ $message  }}</p>
                         @enderror
