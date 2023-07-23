513a0b72f8a8a6e8a1a21702a16889d8478d6548
bphphin <buingocphinn@gmail.com>
2023-07-23 21:48:06 +0700

    feat: add method edit() show form

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -6,59 +6,72 @@ use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use Illuminate\Http\Request;
 use App\Http\Requests\Admin\BrandRequest;
+
 class BrandController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 //        $brands = Brand::paginate(6);
         $brands = Brand::all();
-        return view('admin.pages.brands.index',compact('brands'));
+        return view('admin.pages.brands.index', compact('brands'));
     }
 
-    public function trash() {
+    public function trash()
+    {
         $brands = Brand::onlyTrashed()->get();
-        return view('admin.pages.brands.trash-list',compact('brands'));
+        return view('admin.pages.brands.trash-list', compact('brands'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('admin.pages.brands.create-form');
     }
 
-    public function store(BrandRequest $request) {
+    public function store(BrandRequest $request)
+    {
         $isSuccess = Brand::create([
             'name' => $request->name,
             'slug' => $request->slug ?? '',
             'description' => $request->description ?? '',
         ]);
-        if($isSuccess) {
+        if ($isSuccess) {
 //            alert()->flash('Tạo mới thành công','success');
-            toastr()->success('Thêm mới thành công','success');
+            toastr()->success('Thêm mới thành công', 'success');
             return redirect()->route('admin.brand.index');
         }
-        return back()->withErrors('fail','Thêm mới không thành công');
+        return back()->withErrors('fail', 'Thêm mới không thành công');
     }
 
+    public function edit($id)
+    {
+        $brand = Brand::find($id);
+        return view('admin.pages.brands.edit-form', compact('brand'));
+    }
 
-    public function softDelete($id) {
+    public function softDelete($id)
+    {
 //        $isSuccess = Brand::whereId($id)->forceDelete();
         $isSuccess = Brand::destroy($id);
-        if($isSuccess) {
-            toastr()->success('Xóa thành công','success');
+        if ($isSuccess) {
+            toastr()->success('Xóa thành công', 'success');
             return redirect()->route('admin.brand.index');
         }
     }
 
-    public function restore($id) {
+    public function restore($id)
+    {
         $isSuccess = Brand::onlyTrashed()->whereId($id)->restore();
-        if($isSuccess) {
-            toastr()->success('Cập nhật thành công','success');
+        if ($isSuccess) {
+            toastr()->success('Cập nhật thành công', 'success');
             return redirect()->route('admin.brand.index');
         }
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    {
         $isSuccess = Brand::whereId($id)->forceDelete();
-        if($isSuccess) {
-            toastr()->success('Xóa thành công','success');
+        if ($isSuccess) {
+            toastr()->success('Xóa thành công', 'success');
             return redirect()->route('admin.brand.index');
         }
     }
