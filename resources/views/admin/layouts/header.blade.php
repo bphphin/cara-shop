0e1d07f2a7c7737ebd3ee3fe4b52cfe36afb7b0f
bphphin <buingocphinn@gmail.com>
2023-07-23 15:50:04 +0700

    fix(ui): update header dashboard , display info admin

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,36 +1,14 @@
-{{--begin header--}}
-<header>
-    <input type="checkbox" id="nav-toggle">
-    <div class="sidebar">
-        <div class="sidebar-brand">
-            <h1><a href="{{ route('home-client')  }}"><span>Cara</span></a></h1>
+@include('admin.layouts.side-bar')
+<section class="dashboard">
+    <div class="top">
+        <i class="uil uil-bars sidebar-toggle"></i>
+
+        <div class="search-box">
+            <i class="uil uil-search"></i>
+            <input type="text" placeholder="Search here...">
         </div>
-
-        @include('admin.layouts.side-bar')
-    </div>
-    <h2>
-        <label for="nav-toggle">
-            <span class="fas fa-bars"></span>
-        </label>
-        Dashboard
-    </h2>
-
-    <div class="search-wrapper">
-        <span class="fas fa-search"> </span>
-        <input type="search" placeholder="Search..."/>
-
-    </div>
-
-    <div class="user-wrapper">
         @if(Auth::check())
-            <img src="{{ Auth::user()->avatar  }}" width="40px" height="40px" alt="profile-img">
-            <div class="">
-                <h4>{{ Auth::user()->name  }}</h4>
-                <small>Super Admin</small>
-            </div>
+            <p>{{ Auth::user()->name  }}</p>
+            <img src="{{ Auth::user()->avatar  }}" alt="">
         @endif
-
     </div>
-</header>
-
-{{-- end header --}}
