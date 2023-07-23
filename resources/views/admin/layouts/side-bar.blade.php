dfcfcce6b699dc4f9f99be47e0c245b3af5e9da5
bphphin <buingocphinn@gmail.com>
2023-07-23 15:51:05 +0700

    fix(ui): !important

--- resources/views/admin/layouts/side-bar.blade.php
+++ resources/views/admin/layouts/side-bar.blade.php
@@ -1,52 +1,62 @@
-<div class="sidebar-menu">
-    <ul>
-        <li>
-            <a href="#" class="active">
-                <span class="fas fa-tachometer-alt"></span>
-                <span>Dashboard</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <span class="fas fa-users"></span>
-                <span>Customers</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <span class="fas fa-stream"></span>
-                <span>Projects</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <span class="fas fa-shopping-cart"></span>
-                <span>Orders</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <span class="fas fa-boxes"></span>
-                <span>Inventory</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <span class="fas fa-user-circle"></span>
-                <span>Accounts</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <span class="fas fa-tasks"></span>
-                <span>Task</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('auth.logout')  }}">
-                <span class="fa-solid fa-right-from-bracket"></span>
-                <span>Logout</span>
-            </a>
-        </li>
-    </ul>
-</div>
+<nav>
+    <div class="logo-name">
+{{--        @if(Auth::check())--}}
+{{--        <div class="logo-image">--}}
+{{--            <img src="images/logo.png" alt="">--}}
+{{--            <p>{{ Auth::user()->name  }}</p>--}}
+{{--        </div>--}}
+{{--        @endif--}}
+        <a href="{{ route('home-client')  }}">
+            <span class="logo_name">CaraShop</span>
+        </a>
+    </div>
+
+    <div class="menu-items">
+        <ul class="nav-links">
+            <li><a href="{{ route('admin')  }}">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Dashboard</span>
+                </a></li>
+            <li><a href="{{ route('admin.product.index')  }}">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Sản Phẩm</span>
+                </a></li>
+            <li><a href="{{ route('admin.brand.index')  }}">
+                    <i class="uil uil-chart"></i>
+                    <span class="link-name">Thương Hiệu</span>
+                </a></li>
+            <li><a href="{{ route('admin.category.index')  }}">
+                    <i class="uil uil-thumbs-up"></i>
+                    <span class="link-name">Danh Mục</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="uil uil-comments"></i>
+                    <span class="link-name">Comment</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="uil uil-share"></i>
+                    <span class="link-name">Share</span>
+                </a></li>
+        </ul>
+
+        <ul class="logout-mode">
+            <li><a href="{{ route('auth.logout')  }}">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Logout</span>
+                </a></li>
+
+            <li class="mode">
+{{--                <a href="#">--}}
+{{--                    <i class="uil uil-moon"></i>--}}
+{{--                    <span class="link-name">Dark Mode</span>--}}
+{{--                </a>--}}
+
+                <div class="mode-toggle">
+{{--                    <span class="switch"></span>--}}
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
