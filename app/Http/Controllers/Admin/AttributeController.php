d2ff114c1e41be671ac3cdb472a5948294718637
bphphin <buingocphinn@gmail.com>
2023-07-26 00:39:46 +0700

    feat: render data to view(sizes)

--- app/Http/Controllers/Admin/AttributeController.php
+++ app/Http/Controllers/Admin/AttributeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Size;
 use Illuminate\Http\Request;
 
 class AttributeController extends Controller
 {
     public function index() {
-        return view('admin.pages.attributes.index');
+        $sizes = Size::all();
+        return view('admin.pages.attributes.index')->with('sizes',$sizes);
     }
+
 }
