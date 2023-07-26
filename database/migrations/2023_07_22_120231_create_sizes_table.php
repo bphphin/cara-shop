319c7a0a697a73aa5a5d59215c5c1bff7232a2e6
bphphin <buingocphinn@gmail.com>
2023-07-27 02:15:53 +0700

    refactor: fields desc in tbl_size

--- database/migrations/2023_07_22_120231_create_sizes_table.php
+++ database/migrations/2023_07_22_120231_create_sizes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('sizes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->text('description')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
