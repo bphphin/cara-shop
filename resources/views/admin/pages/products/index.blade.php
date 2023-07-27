dd99f6d9a9139700d60eb810bdcdbf43b87d5834
bphphin <buingocphinn@gmail.com>
2023-07-28 00:23:13 +0700

    feat(ui): create view from Product(hard code)

---
+++ resources/views/admin/pages/products/index.blade.php
@@ -0,0 +1,71 @@
+@extends('admin.layouts.app')
+@section('app')
+    <div class="dash-content">
+        <div class="activity">
+            <div class="py-20">
+                <div class="flex justify-end mb-4">
+                    <a href="{{ route('admin.cate.subcate.store')  }}"
+                       class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-2 rounded-full">+
+                        Product</a>
+                    <a
+                        class="bg-[#f687b3] hover:bg-[#f687b3] text-white font-bold py-2 px-4 rounded-full"
+                        href="{{ route('admin.category.trash')  }}">
+                        Trash
+                        <i class="fa-solid fa-trash"></i>
+                    </a>
+                </div>
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 w-[100px]">
+                                Name
+                            </th>
+                            <th scope="col" class="px-6 py-3 w-[200px]">
+                                Image
+                            </th>
+                            <th scope="col" class="px-6 py-3 w-[480px]">
+                                Description
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                            <td class="px-6 py-4 flex gap-x-4">
+                                <a href=""
+                                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Detail
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
+                                <a href=""
+                                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Edit
+                                    <i class="fa-solid fa-pen"></i>
+                                </a>
+                                <a href=""
+                                   class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Remove
+                                    <i class="fa-solid fa-pen"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
