a061f634a07ac21cd47876e94b0356141b57425e
bphphin <buingocphinn@gmail.com>
2023-07-27 19:34:29 +0700

    fix: to many changes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Admin\SizeController;
 use App\Http\Controllers\Admin\AttributeController;
 use App\Http\Controllers\Admin\ColorController;
 use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\SubCateController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,97 +24,92 @@ use App\Http\Controllers\Admin\CustomerController;
 */
 // Admin
 
-Route::prefix('dashboard')->middleware(['isAdmin','auth'])->group(function() {
+Route::prefix('dashboard')->middleware(['isAdmin', 'auth'])->group(function () {
     // Dashboard
-    Route::get('/',[DashboardController::class,'index'])->name('admin');
+    Route::get('/', [DashboardController::class, 'index'])->name('admin');
 
     //Product
-    Route::prefix('product')->group(function() {
-        Route::get('/',[ProductController::class,'index'])->name('admin.product.index');
+    Route::prefix('product')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('admin.product.index');
     });
 
 
     //Brand
-    Route::prefix('brand')->group(function() {
-        Route::get('/',[BrandController::class,'index'])->name('admin.brand.index');
-        Route::get('trash-list',[BrandController::class,'trash'])->name('admin.brand.trash');
+    Route::prefix('brand')->group(function () {
+        Route::get('/', [BrandController::class, 'index'])->name('admin.brand.index');
+        Route::get('trash-list', [BrandController::class, 'trash'])->name('admin.brand.trash');
 
-        Route::get('create',[BrandController::class,'create'])->name('admin.brand.create');
-        Route::post('create',[BrandController::class,'store'])->name('admin.brand.store');
+        Route::get('create', [BrandController::class, 'create'])->name('admin.brand.create');
+        Route::post('create', [BrandController::class, 'store'])->name('admin.brand.store');
 
-        Route::delete('soft-delete/{id}',[BrandController::class,'softDelete'])->name('admin.brand.softDelete');
-        Route::get('restore/{id}',[BrandController::class,'restore'])->name('admin.brand.restore');
-        Route::delete('delete/{id}',[BrandController::class,'destroy'])->name('admin.brand.destroy');
-        Route::get('edit/{id}',[BrandController::class,'edit'])->name('admin.brand.edit');
-        Route::post('edit/{id}',[BrandController::class,'update'])->name('admin.brand.update');
+        Route::delete('soft-delete/{id}', [BrandController::class, 'softDelete'])->name('admin.brand.softDelete');
+        Route::get('restore/{id}', [BrandController::class, 'restore'])->name('admin.brand.restore');
+        Route::delete('delete/{id}', [BrandController::class, 'destroy'])->name('admin.brand.destroy');
+        Route::get('edit/{id}', [BrandController::class, 'edit'])->name('admin.brand.edit');
+        Route::post('edit/{id}', [BrandController::class, 'update'])->name('admin.brand.update');
     });
 
     // Categories
-    Route::prefix('category')->group(function() {
-       Route::get('/',[CategoryController::class,'index'])->name('admin.category.index');
-       Route::get('trash-list',[CategoryController::class,'trash'])->name('admin.category.trash');
+    Route::prefix('category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('admin.category.index');
+        Route::get('trash-list', [CategoryController::class, 'trash'])->name('admin.category.trash');
 
-       Route::get('create',[CategoryController::class,'create'])->name('admin.category.create');
-       Route::post('create',[CategoryController::class,'store'])->name('admin.category.store');
-       Route::delete('soft-delete/{id}',[CategoryController::class,'softDelete'])->name('admin.category.softDelete');
+        Route::get('create', [CategoryController::class, 'create'])->name('admin.category.create');
+        Route::post('create', [CategoryController::class, 'store'])->name('admin.category.store');
+        Route::delete('soft-delete/{id}', [CategoryController::class, 'softDelete'])->name('admin.category.softDelete');
 //       Route::get('delete/{id}',[CategoryController::class,'destroy'])->name('admin.category.destroy');
-       Route::delete('delete/{id}',[CategoryController::class,'destroy'])->name('admin.category.destroy');
-       Route::get('restore/{id}',[CategoryController::class,'restore'])->name('admin.category.restore');
-       Route::get('edit/{id}',[CategoryController::class,'edit'])->name('admin.category.edit');
-       Route::post('edit/{id}',[CategoryController::class,'update'])->name('admin.category.update');
+        Route::delete('delete/{id}', [CategoryController::class, 'destroy'])->name('admin.category.destroy');
+        Route::get('restore/{id}', [CategoryController::class, 'restore'])->name('admin.category.restore');
+        Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('admin.category.edit');
+        Route::post('edit/{id}', [CategoryController::class, 'update'])->name('admin.category.update');
+
+        Route::prefix('sub-category')->group(function() {
+//            Route::get('/',[SubCateController::class,'index'])->name('admin.cate.subcate.index');
+           Route::match(['GET','POST'],'create',[SubCateController::class,'store'])->name('admin.cate.subcate.store');
+        });
     });
 
 
     //Attribute
-    Route::prefix('attribute')->group(function() {
-        Route::get('/',[AttributeController::class,'index'])->name('admin.att.index');
+    Route::prefix('attribute')->group(function () {
+        Route::get('/', [AttributeController::class, 'index'])->name('admin.att.index');
 
         //Size
-        Route::prefix('size')->group(function() {
+        Route::prefix('size')->group(function () {
 
-            Route::get('/',[SizeController::class,'index'])->name('admin.att.size.index');
-            Route::get('detail/{id}',[SizeController::class,'show'])->name('admin.att.size.show');
-            Route::match(['GET','POST'],'create',[SizeController::class,'store'])->name('admin.att.size.store');
-            Route::delete('delete/{id}',[SizeController::class,'destroy'])->name('admin.att.size.destroy');
+            Route::get('/', [SizeController::class, 'index'])->name('admin.att.size.index');
+            Route::get('detail/{id}', [SizeController::class, 'show'])->name('admin.att.size.show');
+            Route::match(['GET', 'POST'], 'create', [SizeController::class, 'store'])->name('admin.att.size.store');
+            Route::delete('delete/{id}', [SizeController::class, 'destroy'])->name('admin.att.size.destroy');
 //            Route::get('delete/{id}',[SizeController::class,'destroy'])->name('admin.att.size.destroy');
-            Route::match(['GET','POST'],'edit/{id}',[SizeController::class,'update'])->name('admin.att.size.update');
+            Route::match(['GET', 'POST'], 'edit/{id}', [SizeController::class, 'update'])->name('admin.att.size.update');
         });
 
         //Color
-        Route::prefix('color')->group(function() {
-            Route::match(['GET','POST'],'create',[ColorController::class,'store'])->name('admin.att.color.store');
-            Route::match(['GET','POST'],'edit/{id}',[ColorController::class,'update'])->name('admin.att.color.update');
-            Route::delete('delete/{id}',[ColorController::class,'destroy'])->name('admin.att.color.destroy');
+        Route::prefix('color')->group(function () {
+            Route::match(['GET', 'POST'], 'create', [ColorController::class, 'store'])->name('admin.att.color.store');
+            Route::match(['GET', 'POST'], 'edit/{id}', [ColorController::class, 'update'])->name('admin.att.color.update');
+            Route::delete('delete/{id}', [ColorController::class, 'destroy'])->name('admin.att.color.destroy');
         });
     });
-    Route::prefix('customer')->group(function(){
-        Route::get('/',[CustomerController::class,'index'])->name('admin.customer.index');
-        Route::match(['GET','POST'],'edit/{id}',[CustomerController::class,'update'])->name('admin.customer.update');
+    Route::prefix('customer')->group(function () {
+        Route::get('/', [CustomerController::class, 'index'])->name('admin.customer.index');
+        Route::match(['GET', 'POST'], 'edit/{id}', [CustomerController::class, 'update'])->name('admin.customer.update');
     });
 });
 
 
-
-
-
-
-
-
-
-
-
-
 // Clients
-Route::get('/',[HomeController::class,'home'])->name('home-client');
+Route::get('/', [HomeController::class, 'home'])->name('home-client');
 
 
 // Login
 
-Route::get('login',[AuthController::class,'loginForm'])->name('auth.loginForm');
-Route::post('login',[AuthController::class,'login'])->name('auth.login');
+Route::get('login', [AuthController::class, 'loginForm'])->name('auth.loginForm');
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 // Register
-Route::get('register',[AuthController::class,'registerForm'])->name('auth.registerForm');
-Route::post('register',[AuthController::class,'register'])->name('auth.register');
+Route::get('register', [AuthController::class, 'registerForm'])->name('auth.registerForm');
+Route::post('register', [AuthController::class, 'register'])->name('auth.register');
 // Logout
-Route::get('logout',[AuthController::class,'logout'])->name('auth.logout');
-Route::view('not-found','errors.404')->name('404');
+Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
+Route::view('not-found', 'errors.404')->name('404');
