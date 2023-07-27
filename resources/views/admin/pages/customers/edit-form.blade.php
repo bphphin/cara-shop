1a1de093038d66731fd43732a0126dd56409e2ff
bphphin <buingocphinn@gmail.com>
2023-07-27 19:32:21 +0700

    fix: update default value when update role

--- resources/views/admin/pages/customers/edit-form.blade.php
+++ resources/views/admin/pages/customers/edit-form.blade.php
@@ -17,7 +17,7 @@
                         <label for="underline_select" class="sr-only">Underline select</label>
                         <select name="role" id="underline_select"
                                 class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-                            <option selected>Choose a role</option>
+                            <option selected value="0">Choose a role</option>
                             @foreach($roles as $i => $r)
                                 @if($customer->role === $i)
                                     <option value="{{ $i }}" selected>{{ $r }}</option>
