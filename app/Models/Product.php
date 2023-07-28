db809d20d08029468b531f1d7a06454b663895bb
bphphin <buingocphinn@gmail.com>
2023-07-29 00:36:23 +0700

    fix: update fillable tbl Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = 'products';
+    protected $fillable = [
+        'name',
+        'price',
+        'quantity',
+        'slug',
+        'image',
+        'cate_id',
+        'brand_id',
+        'color_id',
+        'size_id',
+        'description'
+    ];
 }
