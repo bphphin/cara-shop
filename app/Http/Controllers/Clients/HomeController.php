8a8c78924ade3be7e5dd6898215b7a9ef7815520
bphphin <buingocphinn@gmail.com>
2023-07-30 02:35:27 +0700

    feat: render data from product  to home

--- app/Http/Controllers/Clients/HomeController.php
+++ app/Http/Controllers/Clients/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Clients;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function home() {
-        return view('clients.pages.home');
+        $products = Product::limit(8)->get();
+        return view('clients.pages.home',compact('products'));
     }
 }
