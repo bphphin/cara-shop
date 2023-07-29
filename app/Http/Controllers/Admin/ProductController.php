2a48c04d36ff8fcc73dfc36c84c3c9bc5e8dc63f
bphphin <buingocphinn@gmail.com>
2023-07-30 01:08:26 +0700

    feat: create function update form Product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,58 +4,101 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Color;
 use App\Models\Product;
 use App\Models\Size;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
+//use RealRashid\SweetAlert\Facades\Alert;
+Use Alert;
 
 class ProductController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $products = Product::paginate(6);
-        return view('admin.pages.products.index',compact('products'));
+        return view('admin.pages.products.index', compact('products'));
     }
 
-    public function trash() {
+    public function trash()
+    {
         $products = Product::onlyTrashed()->paginate(6);
-        return view('admin.pages.products.trash-list',compact('products'));
+        return view('admin.pages.products.trash-list', compact('products'));
     }
 
 
-    public function restore($id) {
+    public function restore($id)
+    {
         $isSuccess = Product::onlyTrashed()->whereId($id)->restore();
-        return checkEndDisplayMsg($isSuccess,'success','Thành công','Hoàn tác thành công','admin.product.index');
+        return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Hoàn tác thành công', 'admin.product.index');
     }
 
-    public function store(Request $request) {
-        $brands = Brand::all();
-        $cates = SubCategory::all();
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
         $colors = Color::all();
         $sizes = Size::all();
-        if($request->method() === 'POST') {
-            if($request->hasFile('image')) {
+        $cates = SubCategory::all();
+        $brands = Brand::all();
+        if ($request->method() === 'POST') {
+            if ($request->hasFile('image')) {
                 $originName = $request->file('image')->getClientOriginalName();
-                $fileName = pathinfo($originName,PATHINFO_FILENAME);
+                $fileName = pathinfo($originName, PATHINFO_FILENAME);
                 $extension = $request->file('image')->getClientOriginalExtension();
-                $image = $fileName . '-' . time() . '.'. $extension;
-                $request->file('image')->move(public_path('upload'),$image);
+                $image = $fileName . '-' . time() . '.' . $extension;
+
             }
-            $isSuccess = Product::create([
+            $isSuccess = Product::where('id', $id)->update([
                 'name' => $request->name,
                 'price' => $request->price,
                 'quantity' => $request->quantity,
                 'slug' => $request->slug,
-                'image' => $image,
+                'image' => $image ?? $product->image,
                 'cate_id' => $request->cate_id,
                 'brand_id' => $request->brand_id,
                 'color_id' => $request->color_id,
                 'size_id' => $request->size_id,
                 'description' => $request->description,
             ]);
-            return checkEndDisplayMsg($isSuccess,'success','Thành công','Thêm mới thành công','admin.product.index');
+            return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Cập nhật thành công', 'admin.product.index');
+            fail('error','Lỗi rồi','Cập nhật không thành công');
         }
-        return view('admin.pages.products.create-form',[
+        return view('admin.pages.products.edit-form', compact('product', 'colors', 'sizes', 'cates', 'brands'));
+    }
+
+    public function store(Request $request)
+    {
+        $brands = Brand::all();
+        $cates = SubCategory::all();
+        $colors = Color::all();
+        $sizes = Size::all();
+        if ($request->method() === 'POST') {
+            if ($request->hasFile('image')) {
+                $originName = $request->file('image')->getClientOriginalName();
+                $fileName = pathinfo($originName, PATHINFO_FILENAME);
+                $extension = $request->file('image')->getClientOriginalExtension();
+                $image = $fileName . '-' . time() . '.' . $extension;
+                $request->file('image')->move(public_path('upload'), $image);
+
+                $isSuccess = Product::create([
+                    'name' => $request->name,
+                    'price' => $request->price,
+                    'quantity' => $request->quantity,
+                    'slug' => $request->slug,
+                    'image' => $image,
+                    'cate_id' => $request->cate_id,
+                    'brand_id' => $request->brand_id,
+                    'color_id' => $request->color_id,
+                    'size_id' => $request->size_id,
+                    'description' => $request->description,
+                ]);
+                return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Thêm mới thành công', 'admin.product.index');
+            }
+
+        }
+        return view('admin.pages.products.create-form', [
             'brands' => $brands,
             'cates' => $cates,
             'colors' => $colors,
@@ -64,9 +107,10 @@ class ProductController extends Controller
     }
 
 
-    public function softDelete($id) {
+    public function softDelete($id)
+    {
         $isSuccess = Product::destroy($id);
-        return checkEndDisplayMsg($isSuccess,'success','Thành công','Xóa thành công','admin.product.index');
+        return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Xóa thành công', 'admin.product.index');
     }
 
 }
