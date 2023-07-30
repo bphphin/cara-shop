13cc81dfd5cc823240d7ac32d8442a8155535fca
bphphin <buingocphinn@gmail.com>
2023-07-31 00:48:57 +0700

    feat: product detail from home

--- app/Http/Controllers/Clients/HomeController.php
+++ app/Http/Controllers/Clients/HomeController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers\Clients;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\Size;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function home() {
-        $products = Product::limit(8)->get();
+        $products = Product::limit(20)->get();
         return view('clients.pages.home',compact('products'));
+    }
+
+    public function showProduct($id,$slug) {
+        $product = Product::find($id);
+        $sizes = Size::all();
+        return view('clients.pages.detail',compact('product','sizes'));
     }
 }
