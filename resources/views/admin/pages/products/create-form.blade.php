ac8428e86691274bda973e8eb351d54636223bf5
bphphin <buingocphinn@gmail.com>
2023-07-31 00:47:13 +0700

    fix(ui): update fields status_id

--- resources/views/admin/pages/products/create-form.blade.php
+++ resources/views/admin/pages/products/create-form.blade.php
@@ -21,10 +21,13 @@
                             </div>
                             <div class="relative z-0 w-full mb-6 group">
                                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload image</label>
-                                <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" name="image">
+                                <input  onchange="changeImg()" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="img" type="file" name="image">
                                 @error('image')
                                 <p class="my-2 text-red-400">{{ $message  }}</p>
                                 @enderror
+                                <div class="my-3 flex justify-center">
+                                    <img src="" alt="" id="imgPreview" width="200px">
+                                </div>
                             </div>
                             <div class="relative z-0 w-full mb-6 group">
                                 <input type="text" name="price" id=""
@@ -52,6 +55,14 @@
                                        placeholder=" " name="slug"/>
                                 <label for=""
                                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Slug</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-6 group">
+                                <label for="" class="block mb-2 text-sm font-medium text-gray-700 dark:text-white">Select an status</label>
+                                <select  name="status_id" id="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    @foreach ($sttProduct as $stt)
+                                        <option value="{{ $stt->id }}">{{ $stt->status }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
 
@@ -130,6 +141,21 @@
         function chooseColor(color) {
             console.log(document.querySelector(`input[name="color_id"]`).value = color);
         }
+
+        //change image
+
+        function changeImg() {
+            const img = document.querySelector('#img').files[0];
+            const imgPreview = document.querySelector('#imgPreview');
+            const reader = new FileReader();
+            reader.addEventListener('load',() => {
+                imgPreview.src = reader.result;
+            });
+            if(img) {
+                reader.readAsDataURL(img);
+            }
+        }
+
         // ck editor
         ClassicEditor
             .create(document.querySelector('#desc'))
