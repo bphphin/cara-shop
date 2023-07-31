ea01ceddb51eddb4496b66eefad36d3d94049a26
bphphin <buingocphinn@gmail.com>
2023-08-01 00:27:39 +0700

    feat: display subCate and default product by Category

--- app/Http/Controllers/Clients/HomeController.php
+++ app/Http/Controllers/Clients/HomeController.php
@@ -12,20 +12,38 @@ use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
+
+    // trang chủ
     public function home() {
         $products = Product::limit(20)->get();
         return view('clients.pages.home',compact('products'));
     }
 
+
+    // detail product
     public function showProduct($id,$slug) {
         $product = Product::find($id);
         $sizes = Size::all();
-        return view('clients.pages.detail',compact('product','sizes'));
+        return view('clients.pages.detail-product',compact('product','sizes'));
     }
 
+
+    // route shop-page
     public function shop() {
         $products = Product::paginate(4);
         $cates = Category::all();
         return view('clients.pages.shop',compact('products','cates'));
+    }
+
+    public function detailCate($id) {
+        $subCate = SubCategory::where('parent_id','=',$id)->get();
+        $productToCate = DB::table('categories')
+            ->leftJoin('sub_categories','sub_categories.parent_id','=','categories.id')
+            ->leftJoin('products','products.cate_id','=','sub_categories.id')
+            ->where('parent_id','=',$id)
+            ->select('sub_categories.name as subCateName','categories.name as cateName','products.*')
+            ->limit(4)->get();
+//        dd($productToCate);
+        return view('clients.pages.detail-category',compact('subCate','productToCate'));
     }
 }
