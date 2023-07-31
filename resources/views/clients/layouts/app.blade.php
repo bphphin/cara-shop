55e5ce1b40834a285b146f8c81dd238e7977f019
bphphin <buingocphinn@gmail.com>
2023-08-01 00:29:38 +0700

    fix(ui): folder partials added

--- resources/views/clients/layouts/app.blade.php
+++ resources/views/clients/layouts/app.blade.php
@@ -6,14 +6,16 @@
     content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','Home Page')</title>
+    {{--  tailwind  --}}
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="icon" href="{{ asset('assets/imgs/clothes-hanger.png') }}" type="image/gif" sizes="16x16">
     <link rel="stylesheet" href="{{ asset('assets/css/clients/home-client.css')  }}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    @include('clients.layouts.header')
+    @include('clients.partials.header')
     @yield('app')
-    @include('clients.layouts.footer')
+    @include('clients.partials.footer')
     @include('sweetalert::alert')
     @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
 </body>
