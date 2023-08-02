93044dd99a882402563090f3fd47feed38280416
bphphin <buingocphinn@gmail.com>
2023-08-02 22:48:48 +0700

    feat: add SiteController for Home

---
+++ app/Http/Controllers/Clients/SiteController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Clients;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Size;
+use App\Models\SubCategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SiteController extends Controller
+{
+    // detail product
+    public function showProduct($id,$slug) {
+        $product = Product::find($id);
+        $sizes = Size::all();
+        return view('clients.pages.detail-product',compact('product','sizes'));
+    }
+
+
+    // shop-page
+    public function shop() {
+        $products = Product::paginate(4);
+        $cates = Category::all();
+        return view('clients.pages.shop',compact('products','cates'));
+    }
+
+    // detail cate end display product default from category
+    public function detailCate($id) {
+        $subCate = SubCategory::where('parent_id','=',$id)->get();
+        $productToCate = DB::table('categories')
+            ->leftJoin('sub_categories','sub_categories.parent_id','=','categories.id')
+            ->leftJoin('products','products.cate_id','=','sub_categories.id')
+            ->where('parent_id','=',$id)
+            ->select('sub_categories.name as subCateName','categories.name as cateName','products.*')
+            ->limit(4)->get();
+//        dd($productToCate);
+        return view('clients.pages.detail-category',compact('subCate','productToCate'));
+    }
+
+    // product from sub cate
+    public function productFromSubCate($id) {
+        $subCate = SubCategory::all();
+        $proFromSubCate = Product::where('cate_id','=',$id)->get();
+        return view('clients.pages.detail-pro-from-subcate',compact('proFromSubCate','subCate'));
+    }
+
+}
