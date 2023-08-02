f94531bbe0998e2ca6678b4a04d2c0938a980643
bphphin <buingocphinn@gmail.com>
2023-08-03 02:46:48 +0700

    fix(ui): update form add to card

--- resources/views/clients/pages/detail-product.blade.php
+++ resources/views/clients/pages/detail-product.blade.php
@@ -7,19 +7,23 @@
         </div>
 
         <div class="single-pro-detail">
-            <h6><a href="{{ route('home-client')  }}" class="text-gray-700">Home</a> / {{ $product->getSubCateName()->name  }}</h6>
-            <h4>{{ $product->name  }}</h4>
-            <h2>{{ number_format($product->price) }}</h2>
-            <select name="size_id" id="" class="border">
-                <option>Select size</option>
-                @foreach($sizes as $size)
-                    <option value="{{ $size->id  }}">{{ $size->name  }}</option>
-                @endforeach
-            </select>
-            <input type="number" value="1" min="1" class="border">
-            <button class="normal">Add To Cart</button>
-            <h4>Product Details</h4>
-            <span>{!! $product->description !!}   </span>
+            <form action="{{ route('site.addToCart') }}" method="POST">
+                @csrf
+                <h6><a href="{{ route('home-client')  }}" class="text-gray-700">Home</a> / {{ $product->getSubCateName()->name  }}</h6>
+                <h4>{{ $product->name  }}</h4>
+                <input type="hidden" value="{{ $product->id }}" name="pro_id">
+                <h2>{{ number_format($product->price) }}</h2>
+                <select name="size_id" id="" class="border">
+                    <option>Select size</option>
+                    @foreach($sizes as $size)
+                        <option value="{{ $size->id  }}">{{ $size->name  }}</option>
+                    @endforeach
+                </select>
+                <input type="number" value="1" min="1" class="border" name="quantity">
+                <button class="normal">Add To Cart</button>
+            </form>
+                <h4>Product Details</h4>
+                <span>{!! $product->description !!}   </span>
         </div>
     </section>
     <section id="comment" class="section-p1">
