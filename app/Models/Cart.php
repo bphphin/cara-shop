5d0a7f651e72ebfe41069666c5e467f402c9eb85
bphphin <buingocphinn@gmail.com>
2023-08-03 02:46:28 +0700

    feat: create model and migration for Cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cart extends Model
+{
+    use HasFactory,SoftDeletes;
+    protected $table = 'carts';
+    protected $fillable = [
+        'pro_id',
+        'user_id',
+        'size_id',
+        'price',
+        'quantity',
+        'total_price',
+    ];
+}
