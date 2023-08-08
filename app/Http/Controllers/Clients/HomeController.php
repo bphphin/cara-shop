1db6146d88d8c0863ae82ee23ce5a16c5d084e6c
bphphin <buingocphinn@gmail.com>
2023-08-08 23:10:06 +0700

    fix: remove count cart

--- app/Http/Controllers/Clients/HomeController.php
+++ app/Http/Controllers/Clients/HomeController.php
@@ -17,15 +17,8 @@ class HomeController extends Controller
 
     // trang chủ
     public function home() {
-        $use_id = Auth::user()->id;
-        $carts = DB::table('carts')
-            ->leftJoin('users','users.id','=','carts.user_id')
-            ->leftJoin('products','products.id','=','carts.pro_id')
-            ->leftJoin('sizes','sizes.id','=','carts.size_id')
-            ->where('user_id','=',$use_id)
-            ->select('carts.*','products.id as pro_id','products.name as proName','products.image','users.name as username')->get();
         $products = Product::limit(20)->get();
-        return view('clients.pages.home',compact('products','carts'));
+        return view('clients.pages.home',compact('products'));
     }
 
 }
