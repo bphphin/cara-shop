3fe02edb8b8eff385be370994d4724133ce32a07
bphphin <buingocphinn@gmail.com>
2023-11-11 00:43:33 +0700

    refactor ProductController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -9,15 +9,17 @@ use App\Models\Product;
 use App\Models\Size;
 use App\Models\StatusProduct;
 use App\Models\SubCategory;
-use Illuminate\Http\Request;
-Use Alert;
+use Alert;
 use App\Http\Requests\Admin\ProductRequest;
-use Illuminate\Support\Str;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 class ProductController extends Controller
 {
+
+    private const PATH_UPLOAD = 'admin/products';
+
     public function index()
     {
-        $products = Product::paginate(6);
+        $products = Product::latest()->paginate(6);
         return view('admin.pages.products.index', compact('products'));
     }
 
@@ -33,52 +35,31 @@ class ProductController extends Controller
         return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Hoàn tác thành công', 'admin.product.index');
     }
 
-
-    public function edit($id) {
-        $product = Product::find($id);
-        $colors = Color::all();
-        $sizes = Size::all();
-        $cates = SubCategory::all();
-        $brands = Brand::all();
-        $sttProduct = StatusProduct::all();
-        return view('admin.pages.products.edit-form', compact('product', 'colors', 'sizes', 'cates', 'brands','sttProduct'));
-    }
-
-    public function update(ProductRequest $request, $id)
+    public function store(ProductRequest $request)
     {
-        $product = Product::find($id);
-        if ($request->method() === 'POST') {
-            if ($request->hasFile('image')) {
-                $originName = $request->file('image')->getClientOriginalName();
-                $fileName = pathinfo($originName, PATHINFO_FILENAME);
-                $extension = $request->file('image')->getClientOriginalExtension();
-                $image = $fileName . '-' . time() . '.' . $extension;
-
-                $isSuccess = Product::where('id', $id)->update([
-                    'name' => $request->name,
-                    'price' => $request->price,
-                    'quantity' => $request->quantity,
-                    'slug' => Str::slug($request->name),
-                    'image' => $image ?? $product->image,
-                    'cate_id' => $request->cate_id ?? $product->cate_id,
-                    'brand_id' => $request->brand_id ?? $product->brand_id,
-                    'color_id' => $request->color_id ?? $product->color_id,
-                    'size_id' => $request->size_id ?? $product->size_id,
-                    'status_id' => $request->status_id ?? $product->status_id,
-                    'description' => $request->description,
-                ]);
-                return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Cập nhật thành công', 'admin.product.index');
-            }
-        }
-    }
-
-
-    public function create() {
         $brands = Brand::all();
         $cates = SubCategory::all();
         $colors = Color::all();
         $sizes = Size::all();
         $sttProduct = StatusProduct::all();
+        if ($request->method() === 'POST') {
+
+            $data = $request->except('image');
+
+            if($request->hasFile('image')) {
+                $data['image'] = Cloudinary::upload($request->file('image')->getRealPath(),array(
+                    'folder' => 'Cara/Products',
+                    'overwrite' => TRUE,
+                    'resource_type' => 'image'
+                ))->getSecurePath();
+            }
+            $isSuccess = Product::create($data);
+            if(!$isSuccess) {
+                $publicId = $this->getPublicId($data['image']);
+                Cloudinary::destroy($publicId);
+            }
+            return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Thêm mới thành công', 'admin.product.index');
+        }
         return view('admin.pages.products.create-form', [
             'brands' => $brands,
             'cates' => $cates,
@@ -88,35 +69,37 @@ class ProductController extends Controller
         ]);
     }
 
-    public function store(ProductRequest $request)
+    public function update(ProductRequest $request, $id)
     {
+        $product = Product::find($id);
+        $colors = Color::all();
+        $sizes = Size::all();
+        $cates = SubCategory::all();
+        $brands = Brand::all();
+        $sttProduct = StatusProduct::all();
         if ($request->method() === 'POST') {
-            if ($request->hasFile('image')) {
-                $originName = $request->file('image')->getClientOriginalName();
-                $fileName = pathinfo($originName, PATHINFO_FILENAME);
-                $extension = $request->file('image')->getClientOriginalExtension();
-                $image = $fileName . '-' . time() . '.' . $extension;
-                $request->file('image')->move(public_path('upload'), $image);
 
-                $isSuccess = Product::create([
-                    'name' => $request->name,
-                    'price' => $request->price,
-                    'quantity' => $request->quantity,
-                    'slug' => Str::slug($request->name),
-                    'image' => $image,
-                    'cate_id' => $request->cate_id,
-                    'brand_id' => $request->brand_id,
-                    'color_id' => $request->color_id,
-                    'size_id' => $request->size_id,
-                    'status_id' => $request->status_id,
-                    'description' => $request->description,
-                ]);
-                return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Thêm mới thành công', 'admin.product.index');
+            $data = $request->except(['_token','image']);
+
+            if($request->hasFile('image')) {
+                $data['image'] = $data['image'] = Cloudinary::upload($request->file('image')->getRealPath(),array(
+                    'folder' => 'Cara/Products',
+                    'overwrite' => TRUE,
+                    'resource_type' => 'image'
+                ))->getSecurePath();
             }
 
-        }
-    }
+            $isSuccess = Product::where('id', $id)->update($data);
 
+            if(!$isSuccess) {
+                $publicId = $this->getPublicId($data['image']);
+                Cloudinary::destroy($publicId);
+            }
+
+            return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Cập nhật thành công', 'admin.product.index');
+        }
+        return view('admin.pages.products.edit-form', compact('product', 'colors', 'sizes', 'cates', 'brands', 'sttProduct'));
+    }
 
     public function softDelete($id)
     {
@@ -124,8 +107,9 @@ class ProductController extends Controller
         return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Xóa thành công', 'admin.product.index');
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    {
         $isSuccess = Product::whereId($id)->forceDelete();
-        return checkEndDisplayMsg($isSuccess,'success','Thành công','Xóa thành công','admin.product.index');
+        return checkEndDisplayMsg($isSuccess, 'success', 'Thành công', 'Xóa thành công', 'admin.product.index');
     }
 }
