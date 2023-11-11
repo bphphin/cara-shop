f164fba333d1c4b079d859ebb6bbd4a27ec4f841
bphphin <buingocphinn@gmail.com>
2023-11-11 14:54:30 +0700

    update script

--- resources/views/clients/layouts/app.blade.php
+++ resources/views/clients/layouts/app.blade.php
@@ -18,5 +18,6 @@
     @include('clients.partials.footer')
     @include('sweetalert::alert')
     @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
+    @stack('script')
 </body>
 </html>
