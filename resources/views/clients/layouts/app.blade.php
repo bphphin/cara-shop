4c0e75bab3e874b42e9a6bfd22b4f7854e478308
bphphin <buingocphinn@gmail.com>
2023-11-11 13:57:30 +0700

    fix: update title

--- resources/views/clients/layouts/app.blade.php
+++ resources/views/clients/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
     content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','Home Page')</title>
+    <title>@yield('title','Trang Chủ')</title>
     {{--  tailwind  --}}
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="icon" href="{{ asset('assets/imgs/clothes-hanger.png') }}" type="image/gif" sizes="16x16">
