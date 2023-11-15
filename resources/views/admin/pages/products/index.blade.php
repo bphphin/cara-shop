2655caba278bfa5bc15fb787d12b75179b200f4b
bphphin <buingocphinn@gmail.com>
2023-11-15 10:51:16 +0700

    fix(ui)

--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -34,7 +34,7 @@
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
 
                                 <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white truncate">
                                     {{ $pro->name  }}
                                 </th>
                                 <td class="px-6 py-4">
